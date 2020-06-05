558dfa7de90b6337d7be0ca55439576c927b2e6a
jmangaru <44428866+jmangaru@users.noreply.github.com>
2020-06-05 21:04:12 +0300

    logo and webview

--- app/src/main/java/com/example/gncc/MainActivity.java
+++ app/src/main/java/com/example/gncc/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.gncc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String Url = "https://gncc-community.co.ke";
+
+        webView = (WebView) findViewById(R.id.gnccapp);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(Url);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()){
+            webView.goBack();
+        }
+        else
+        super.onBackPressed();
     }
 }
